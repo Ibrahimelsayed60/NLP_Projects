2a9a93e142cdc9394548db806ec76af7b4d4d396
Ibrahimelsayed60 <hemasayed600@gmail.com>
2022-07-22 16:23:15 +0200

    Markov Model

---
+++ MarkovModel/src/PosTagging.py
@@ -0,0 +1,208 @@
+from utils_pos import get_word_tag, preprocess
+import pandas as pd
+import math
+import numpy as np
+from collections import defaultdict
+
+
+with open ("WSJ_02-21.pos",'r') as f:
+    training_corpus = f.readlines()
+
+# print(training_corpus)
+
+print(f"A few items of the training corpus list")
+print(training_corpus[0:5])
+
+with open("hmm_vocab.txt",'r') as f:
+    voc_l = f.read().split('\n')
+
+print("A few items of the vocabulary list")
+print(voc_l[0:50])
+print()
+print("A few items at the end of the vocabulary list")
+print(voc_l[-50:])
+
+vocab = {}
+for i, word in enumerate(sorted(voc_l)):
+    vocab[word] = i
+
+print("Vocabulary dictionary, key is the eord, value is a unique integer")
+
+# print(vocab)
+
+cut = 0
+for k,v in vocab.items():
+    print(f"{k}:{v}")
+    cut += 1
+    if cut > 20:
+        break
+
+with open("WSJ_24.pos", 'r') as f:
+    y = f.readlines()
+
+print("A sample of the test corpus")
+print(y[0:10])
+
+_, prep = preprocess(vocab, "test.words")
+print('The length of the preprocessed test corpus: ', len(prep))
+print('This is a sample of the test_corpus: ')
+print(prep[0:10])
+
+
+
+
+def create_dictionaries(training_corpus, vocab):
+    """
+    Input: 
+        training_corpus: a corpus where each line has a word followed by its tag.
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+    Output: 
+        emission_counts: a dictionary where the keys are (tag, word) and the values are the counts
+        transition_counts: a dictionary where the keys are (prev_tag, tag) and the values are the counts
+        tag_counts: a dictionary where the keys are the tags and the values are the counts
+    """
+    
+    # initialize the dictionaries using defaultdict
+    emission_counts = defaultdict(int)
+    transition_counts = defaultdict(int)
+    tag_counts = defaultdict(int)
+    
+    # Initialize "prev_tag" (previous tag) with the start state, denoted by '--s--'
+    prev_tag = '--s--' 
+    
+    # use 'i' to track the line number in the corpus
+    i = 0 
+    
+    # Each item in the training corpus contains a word and its POS tag
+    # Go through each word and its tag in the training corpus
+    for word_tag in training_corpus:
+        
+        # Increment the word_tag count
+        i += 1
+        
+        # Every 50,000 words, print the word count
+        if i % 50000 == 0:
+            print(f"word count = {i}")
+            
+        ### START CODE HERE (Replace instances of 'None' with your code) ###
+        # get the word and tag using the get_word_tag helper function (imported from utils_pos.py)
+        word, tag = get_word_tag(word_tag,vocab) 
+        
+        # Increment the transition count for the previous word and tag
+        transition_counts[(prev_tag, tag)] += 1
+        
+        # Increment the emission count for the tag and word
+        emission_counts[(tag, word)] += 1
+
+        # Increment the tag count
+        tag_counts[tag] += 1
+
+        # Set the previous tag to this tag (for the next iteration of the loop)
+        prev_tag = tag
+        
+        ### END CODE HERE ###
+        
+    return emission_counts, transition_counts, tag_counts
+
+emission_counts, transition_counts, tag_counts = create_dictionaries(training_corpus, vocab)
+states = sorted(tag_counts.keys())
+print(f"Number of POS tags (number of 'states'): {len(states)}")
+print("View these POS tags (states)")
+print(states)
+
+
+print("transition examples: ")
+for ex in list(transition_counts.items())[:3]:
+    print(ex)
+print()
+
+print("emission examples: ")
+for ex in list(emission_counts.items())[200:203]:
+    print (ex)
+print()
+
+print("ambiguous word example: ")
+for tup,cnt in emission_counts.items():
+    if tup[1] == 'back': print (tup, cnt) 
+
+
+# UNQ_C2 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: predict_pos
+
+def predict_pos(prep, y, emission_counts, vocab, states):
+    '''
+    Input: 
+        prep: a preprocessed version of 'y'. A list with the 'word' component of the tuples.
+        y: a corpus composed of a list of tuples where each tuple consists of (word, POS)
+        emission_counts: a dictionary where the keys are (tag,word) tuples and the value is the count
+        vocab: a dictionary where keys are words in vocabulary and value is an index
+        states: a sorted list of all possible tags for this assignment
+    Output: 
+        accuracy: Number of times you classified a word correctly
+    '''
+    
+    # Initialize the number of correct predictions to zero
+    num_correct = 0
+    
+    # Get the (tag, word) tuples, stored as a set
+    all_words = set(emission_counts.keys())
+    
+    # Get the number of (word, POS) tuples in the corpus 'y'
+    total = len(y)
+    for word, y_tup in zip(prep, y): 
+
+        # Split the (word, POS) string into a list of two items
+        y_tup_l = y_tup.split()
+        
+        # Verify that y_tup contain both word and POS
+        if len(y_tup_l) == 2:
+            
+            # Set the true POS label for this word
+            true_label = y_tup_l[1]
+
+        else:
+            # If the y_tup didn't contain word and POS, go to next word
+            continue
+    
+        count_final = 0
+        pos_final = ''
+        
+        # If the word is in the vocabulary...
+        if word in vocab:
+            for pos in states:
+
+            ### START CODE HERE (Replace instances of 'None' with your code) ###
+                        
+                # define the key as the tuple containing the POS and word
+                key = (pos,word)
+
+                # check if the (pos, word) key exists in the emission_counts dictionary
+                if key in emission_counts: # complete this line
+
+                # get the emission count of the (pos,word) tuple 
+                    count = emission_counts[key]
+
+                    # keep track of the POS with the largest count
+                    if count > count_final: # complete this line
+
+                        # update the final count (largest count)
+                        count_final =count
+
+                        # update the final POS
+                        pos_final = pos
+
+            # If the final POS (with the largest count) matches the true POS:
+            if pos_final == true_label: # complete this line
+                
+                # Update the number of correct predictions
+                num_correct += 1
+            
+    ### END CODE HERE ###
+    accuracy = num_correct / total
+    
+    return accuracy
+
+accuracy_predict_pos = predict_pos(prep, y, emission_counts, vocab, states)
+print(f"Accuracy of prediction using predict_pos is {accuracy_predict_pos:.4f}")
+
+
